f2eb665598b0df471fa8b391bb501824c885f758
vetalana <54117600+vetalana@users.noreply.github.com>
2019-09-17 21:59:35 +0300

    Add 2 17/09/2019

--- MyAttempt/src/test/java/wiki/TestBase.java
+++ MyAttempt/src/test/java/wiki/TestBase.java
@@ -6,12 +6,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import java.util.concurrent.TimeUnit;
+
 public class TestBase {
     WebDriver driver;
 
     @BeforeClass
     public void setUp() {
         driver = new ChromeDriver();
+        //add default wait for WebElement
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
         //open site
         driver.get("https://en.wikipedia.org");
     }

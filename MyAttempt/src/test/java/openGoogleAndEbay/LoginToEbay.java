9a8cd1e8c6696cc14acee100f8dd749e16326847
vetalana <54117600+vetalana@users.noreply.github.com>
2019-09-19 13:40:54 +0300

    Added 19.09.2019

--- MyAttempt/src/test/java/openGoogleAndEbay/LoginToEbay.java
+++ MyAttempt/src/test/java/openGoogleAndEbay/LoginToEbay.java
@@ -3,28 +3,14 @@ package openGoogleAndEbay;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-public class LoginToEbay extends TestBase{
+public class LoginToEbay extends TestBase {
     @Test
     public void logInTest(){
-        driver.get("https://www.ebay.com");
-
-        click(By.id("gh-ug"));
-
-        click(By.id("userid"));
-        driver.findElement(By.id("userid")).clear();
-        driver.findElement(By.id("userid")).sendKeys("svetlana.portnaia@gmail.com");
-
-        click(By.id("pass"));
-        driver.findElement(By.id("pass")).clear();
-        driver.findElement(By.id("pass")).sendKeys("123456789");
-
-        click(By.id("sgnBt"));
-
-
+        openSite("https://www.ebay.com");
+        initLogin();
+        fillInLoginForm("svetlana.portnaia@gmail.com", "123456789");
+        clickConfirmLoginButton();
     }
 
-    public void click(By locator) {
-        driver.findElement(locator).click();
-    }
 
 }
--- MyAttempt/src/test/java/openGoogleAndEbay/TestBase.java
+++ MyAttempt/src/test/java/openGoogleAndEbay/TestBase.java
@@ -1,5 +1,6 @@
 package openGoogleAndEbay;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -20,5 +21,30 @@ public class TestBase {
     @AfterMethod
     public void tearDown() {
         driver.quit();
+    }
+    public void click(By locator) {
+        driver.findElement(locator).click();
+    }
+    public void type(By locator, String text) {
+        click(locator);
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    public void openSite(String url) {
+        driver.get(url);
+    }
+
+    public void clickConfirmLoginButton() {
+        click(By.id("sgnBt"));
+    }
+
+    public void fillInLoginForm(String mail, String password) {
+        type(By.id("userid"), mail);
+        type(By.id("pass"), password);
+    }
+
+    public void initLogin() {
+        click(By.id("gh-ug"));
     }
 }
